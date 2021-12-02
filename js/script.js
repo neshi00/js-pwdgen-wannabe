883f331c0773fd4c465e5863fdfcb6a2869de1d3
neshi00 <luca.ascione85@gmail.com>
2021-12-02 15:06:41 +0100

    variabile nome - cognome - colore

--- js/script.js
+++ js/script.js
@@ -1,20 +1,11 @@
 // chiedi nome con prompt (username)
-
-
-
-
+let userName = prompt ('inserisci qui il tuo nome');
 
 // chiedi cognome con prompt (userlastname)
-
-
-
-
-
+let userLastname = prompt ('inserisci qui il tuo cognome');
 
 // chiedi colore preferito con prompt (favourite color)
-
-
-
+let favouriteColor = prompt ('inserisci qui il tuo colore preferito');
 
 
 // concatena stringa "21" a variabili precenti

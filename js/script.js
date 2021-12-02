8b64bf540d6b6effcefb99f23c87e5948365f8b9
neshi00 <luca.ascione85@gmail.com>
2021-12-02 14:59:56 +0100

    struttura js commentata

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// chiedi nome con prompt (username)
+
+
+
+
+
+// chiedi cognome con prompt (userlastname)
+
+
+
+
+
+
+// chiedi colore preferito con prompt (favourite color)
+
+
+
+
+
+// concatena stringa "21" a variabili precenti
+
+
+
+// scrivi in DOM attraverso id #passwgen -> nomecognomecolorepreferito21
